090705a52cf6a1ad7a806149d51119aa2a8f515a
Andrew McLeod <apmcleod@gmail.com>
2019-06-21 18:32:03 +0900

    Triplets working. TODO: MIN_NOTE_LENGTH

--- src/metalign/hierarchy/lpcfg/MetricalLpcfgTreeFactory.java
+++ src/metalign/hierarchy/lpcfg/MetricalLpcfgTreeFactory.java
@@ -19,8 +19,6 @@ import metalign.utils.MidiNote;
  * @author Andrew McLeod - 25 April, 2016
  */
 public class MetricalLpcfgTreeFactory {
-	private static int SUB_BEAT_LENGTH = Main.SUB_BEAT_LENGTH;
-	
 	/**
 	 * Private constructor to ensure that no factory is instantiated.
 	 */
@@ -86,11 +84,10 @@ public class MetricalLpcfgTreeFactory {
 	}
 
 	/**
-	 * Make and return a {@link MetricalLpcfgNonterminal} node corresponding to a beat. If the given
-	 * beatBeats List contains sub beats, this method will return a node using them. Otherwise,
-	 * this will check both 2 and 3 sub-beats and return the one which seems to match the underlying
-	 * notes the best, with a preference towards the number of sub-beats per beat dictated by the
-	 * given measure.
+	 * Make and return a {@link MetricalLpcfgNonterminal} node corresponding to a beat. This will try
+	 * both 2 and 3 sub-beats and return the one which seems to match the underlying
+	 * notes the best, with a preference towards the number of sub-beats given in beatBeats (if any),
+	 * of the number of sub beats per beat dictated by the given measure (otherwise).
 	 * 
 	 * @param measure The measure structure for this beat.
 	 * @param prevTimeReturn The last tatum time from the previous beat. This value will be edited in place to
@@ -109,129 +106,374 @@ public class MetricalLpcfgTreeFactory {
 	private static MetricalLpcfgNode makeBeat(Measure measure, List<Long> prevTimeReturn, List<Beat> beatBeats,
 			long nextTime, List<MidiNote> beatNotes, boolean hasStarted) throws MalformedTreeException {
 		
-		if (beatBeats.size() == measure.getBeatsPerBar()) {
-			// No sub-beats given, try splitting into both 2 and 3.
+		// Try splitting into both 2 and 3.
+		
+		// Try in 2
+		List<Long> timesIn2In3 = getAnchoredTimes(beatBeats, 6, nextTime, beatBeats.get(0).getTime() - prevTimeReturn.get(0));
+		List<Long> timesIn2In4 = getAnchoredTimes(beatBeats, 8, nextTime, beatBeats.get(0).getTime() - prevTimeReturn.get(0));
+		
+		MetricalLpcfgNode in2In3 = makeBeat(prevTimeReturn.get(0), timesIn2In3, nextTime, beatNotes, hasStarted, 3);
+		MetricalLpcfgNode in2In4 = makeBeat(prevTimeReturn.get(0), timesIn2In4, nextTime, beatNotes, hasStarted, 4);
+		
+		// Try in 3
+		List<Long> timesIn3In3 = getAnchoredTimes(beatBeats, 9, nextTime, beatBeats.get(0).getTime() - prevTimeReturn.get(0));
+		List<Long> timesIn3In4 = getAnchoredTimes(beatBeats, 12, nextTime, beatBeats.get(0).getTime() - prevTimeReturn.get(0));
+		
+		MetricalLpcfgNode in3In3 = makeBeat(prevTimeReturn.get(0), timesIn3In3, nextTime, beatNotes, hasStarted, 3);
+		MetricalLpcfgNode in3In4 = makeBeat(prevTimeReturn.get(0), timesIn3In4, nextTime, beatNotes, hasStarted, 4);
+		
+		// Measure well-formedness of each and return the best one.
+		
+		// Measure average distance from note onset to tatum
+		double avgDiff23 = 0.0;
+		double avgDiff24 = 0.0;
+		double avgDiff33 = 0.0;
+		double avgDiff34 = 0.0;
+		int count = 0;
+		
+		for (MidiNote note : beatNotes) {
+			long time = note.getOnsetTime();
 			
-			// Try in 2
-			List<Long> timesIn2 = new ArrayList<Long>(SUB_BEAT_LENGTH * 2);
-			
-			double diff = ((double) nextTime - beatBeats.get(0).getTime()) / (SUB_BEAT_LENGTH * 2);
-			if (nextTime == Long.MAX_VALUE) {
-				// Fix for no following bar: keep previous bar's tatum length 
-				diff = beatBeats.get(0).getTime() - prevTimeReturn.get(0);
+			// Skip notes not in this bar
+			if (time < timesIn2In3.get(0) - 10000) {
+				continue;
+			}
+			if (time > nextTime - 10000) {
+				break;
 			}
 			
-			for (int i = 0; i < SUB_BEAT_LENGTH * 2; i++) {
-				timesIn2.add(Math.round(beatBeats.get(0).getTime() + i * diff));
-			}
-			MetricalLpcfgNode in2 = makeBeat(prevTimeReturn.get(0), timesIn2, nextTime, beatNotes, hasStarted);
+			count++;
 			
-			// Try in 3
-			List<Long> timesIn3 = new ArrayList<Long>(SUB_BEAT_LENGTH * 3);
+			int closestIndex23 = MathUtils.linearSearch(timesIn2In3, time, 0);
+			int closestIndex24 = MathUtils.linearSearch(timesIn2In4, time, 0);
+			int closestIndex33 = MathUtils.linearSearch(timesIn3In3, time, 0);
+			int closestIndex34 = MathUtils.linearSearch(timesIn3In4, time, 0);
 			
-			diff = ((double) nextTime - beatBeats.get(0).getTime()) / (SUB_BEAT_LENGTH * 3);
-			if (nextTime == Long.MAX_VALUE) {
-				// Fix for no following bar: keep previous bar's tatum length 
-				diff = beatBeats.get(0).getTime() - prevTimeReturn.get(0);
-			}
-			
-			for (int i = 0; i < SUB_BEAT_LENGTH * 3; i++) {
-				timesIn3.add(Math.round(beatBeats.get(0).getTime() + i * diff));
-			}
-			MetricalLpcfgNode in3 = makeBeat(prevTimeReturn.get(0), timesIn3, nextTime, beatNotes, hasStarted);
-			
-			// Measure well-formedness of each and return the best one.
-			
-			// Measure average distance from note onset to tatum
-			double avgDiff2 = 0.0;
-			double avgDiff3 = 0.0;
-			int count = 0;
-			
-			for (MidiNote note : beatNotes) {
-				long time = note.getOnsetTime();
-				
-				if (time < timesIn2.size() - 10000 || time > nextTime - 10000) {
-					continue;
-				}
-				
-				count++;
-				
-				int closestIndex2 = MathUtils.linearSearch(timesIn2, time, 0);
-				int closestIndex3 = MathUtils.linearSearch(timesIn3, time, 0);
-				
-				avgDiff2 += Math.abs(timesIn2.get(closestIndex2) - time);
-				avgDiff3 += Math.abs(timesIn3.get(closestIndex3) - time);
-			}
-			
-			avgDiff2 /= count;
-			avgDiff3 /= count;
-			
-			// Get best (assume in2, it's more common)
-			List<Long> bestTimes = timesIn2;
-			MetricalLpcfgNode bestNode = in2;
-			if ((measure.getSubBeatsPerBeat() == 3 && avgDiff2 < 0.5 * avgDiff3) ||
-				(measure.getSubBeatsPerBeat() == 2 && avgDiff3 < 0.5 * avgDiff2)) {
-				
-				// 3 splits is the best choice
-				bestTimes = timesIn3;
-				bestNode = in3;
-			}
-			
-			prevTimeReturn.set(0, bestTimes.get(bestTimes.size() - 1));
-			return bestNode;
+			avgDiff23 += Math.abs(timesIn2In3.get(closestIndex23) - time);
+			avgDiff24 += Math.abs(timesIn2In4.get(closestIndex24) - time);
+			avgDiff33 += Math.abs(timesIn3In3.get(closestIndex33) - time);
+			avgDiff34 += Math.abs(timesIn3In4.get(closestIndex34) - time);
 		}
 		
-		// Here, sub beats are given. We will trust them.
+		avgDiff23 /= count;
+		avgDiff24 /= count;
+		avgDiff33 /= count;
+		avgDiff34 /= count;
 		
-		// Get beats
-		List<Integer> subBeatIndices = new ArrayList<Integer>();
-		for (int i = 0; i < beatBeats.size(); i++) {
-			if (beatBeats.get(i).isSubBeat()) {
-				subBeatIndices.add(i);
-			}
+		// TODO: Handle triplet anchors in duple bar
+		
+		// Order of preference:
+		// 1. Number of subbeats as given by anchors. (With 4 sub beats).
+		// 2. Number of sub beats as given by measure type. (With 4 sub beats).
+		// 3-4. 1 and 2, with 3 sub beats.
+		// 5-8. 1-4, but with the other number of sub beats (if not already used).
+		
+		// Idea: First, check best in2 and in3.
+		// Then, compare those 2
+		
+		// In2 check
+		MetricalLpcfgNode bestNode2 = in2In4;
+		List<Long> bestTimes2 = timesIn2In4;
+		double bestDiff2 = avgDiff24;
+		
+		if (avgDiff23 < 0.5 * avgDiff24) {
+			
+			// 2 triplet sub beats is the best choice
+			bestNode2 = in2In3;
+			bestTimes2 = timesIn2In3;
+			bestDiff2 = avgDiff23;
 		}
 		
-		if (subBeatIndices.size() != 2 && subBeatIndices.size() != 3) {
-			throw new MalformedTreeException("Expected 2 or 3 sub-beats per bar, but given "
-					+ subBeatIndices.size() + " (from beat tracker).");
+		// In3 check
+		MetricalLpcfgNode bestNode3 = in3In4;
+		List<Long> bestTimes3 = timesIn3In4;
+		double bestDiff3 = avgDiff34;
+		
+		if (avgDiff33 < 0.5 * avgDiff34) {
+			
+			// 2 triplet sub beats is the best choice
+			bestNode3 = in3In3;
+			bestTimes3 = timesIn3In3;
+			bestDiff3 = avgDiff33;
 		}
 		
-		// TODO: Ignore given subbeats ?
+		// Check for preference between best 2 and best 3
+		MetricalLpcfgNode best;
+		List<Long> bestTimes;
 		
-		// Go through each sub-beat, creating its times
-		List<Long> times = new ArrayList<Long>();
-		for (int subBeatNum = 0; subBeatNum < subBeatIndices.size(); subBeatNum++) {
-			int numAnchors = (subBeatNum == subBeatIndices.size() - 1 ? beatBeats.size() : subBeatIndices.get(subBeatNum + 1))
-					- subBeatIndices.get(subBeatNum);
+		if (bestDiff2 < 0.5 * bestDiff3) {
+			// 2 is clearly better
+			bestTimes = bestTimes2;
+			best = bestNode2;
 			
-			if (SUB_BEAT_LENGTH % numAnchors != 0) {
-				throw new MalformedTreeException("The number of anchor points between consecutive sub-beats (" + numAnchors +
-						", from beat tracker) does not divide SUB_BEAT_LENGTH (" + SUB_BEAT_LENGTH + ")");
-			}
+		} else if (bestDiff3 < 0.5 * bestDiff2) {
+			// 3 is clearly better
+			bestTimes = bestTimes3;
+			best = bestNode3;
 			
-			int tatumsBetweenAnchors = SUB_BEAT_LENGTH / numAnchors;
-			// Pre-load this to allow for saving previous tatum in case of nextTime = Long.MAX_VALUE
-			double diff = beatBeats.get(0).getTime() - prevTimeReturn.get(0);
-			
-			// Go through each anchor Beat in beatBeats
-			for (int anchor = 0; anchor < numAnchors; anchor++) {
-				long startTime = beatBeats.get(subBeatIndices.get(subBeatNum) + anchor).getTime();
-				long endTime = subBeatNum < subBeatIndices.size() - 1 ?
-									beatBeats.get(subBeatIndices.get(subBeatNum + 1)).getTime() : nextTime;
-				
-				if (endTime != Long.MAX_VALUE) {
-					diff = ((double) endTime - startTime) / tatumsBetweenAnchors;
-				}
-				
-				for (int tatum = 0; tatum < tatumsBetweenAnchors; tatum++) {
-					times.add((long) (startTime + diff * tatum));
+		} else {
+			// Check which one's quantum can be reduced to the shortest length
+			List<MetricalLpcfgQuantum> quantum2 = new ArrayList<MetricalLpcfgQuantum>();
+			if (bestNode2 instanceof MetricalLpcfgTerminal) {
+				quantum2 = reduceQuantum(bestNode2.getQuantum());
+			} else {
+				for (MetricalLpcfgNode node : ((MetricalLpcfgNonterminal) bestNode2).getChildren()) {
+					quantum2.addAll(reduceQuantum(node.getQuantum()));
 				}
 			}
+			
+			List<MetricalLpcfgQuantum> quantum3 = new ArrayList<MetricalLpcfgQuantum>();
+			if (bestNode3 instanceof MetricalLpcfgTerminal) {
+				quantum3 = reduceQuantum(bestNode3.getQuantum());
+			} else {
+				for (MetricalLpcfgNode node : ((MetricalLpcfgNonterminal) bestNode3).getChildren()) {
+					quantum3.addAll(reduceQuantum(node.getQuantum()));
+				}
+			}
+			
+			best = bestNode2;
+			bestTimes = bestTimes2;
+			if (quantum3.size() < quantum2.size() || (quantum3.size() == quantum2.size() && measure.getSubBeatsPerBeat() == 3)) {
+				// triplet reduces more OR they reduce equally, but triplet is natural for the bar.
+				best = bestNode3;
+				bestTimes = bestTimes3;
+			}
 		}
 		
-		MetricalLpcfgNode beat = makeBeat(prevTimeReturn.get(0), times, nextTime, beatNotes, hasStarted);
-		prevTimeReturn.set(0, times.get(times.size() - 1));
-		return beat;
+		
+		prevTimeReturn.set(0, bestTimes.get(bestTimes.size() - 1));
+		return best;
+	}
+	
+	/**
+	 * Convert the given pattern into reduced form and return it. That is, divide all constituent
+	 * lengths by their GCF.
+	 * 
+	 * @param beatQuantum The pattern we want to reduce.
+	 * @return The given pattern in fully reduced form.
+	 */
+	private static List<MetricalLpcfgQuantum> reduceQuantum(List<MetricalLpcfgQuantum> beatQuantum) {
+		int gcf = getGCF(beatQuantum);
+		
+		List<MetricalLpcfgQuantum> reducedPattern = new ArrayList<MetricalLpcfgQuantum>(beatQuantum.size() / gcf);
+		
+		// Are we initially in a rest?
+		boolean inRest = beatQuantum.get(0) == MetricalLpcfgQuantum.REST;
+		int currentLength = 1;
+		
+		for (int i = 1; i < beatQuantum.size(); i++) {
+			switch (beatQuantum.get(i)) {
+			case REST:
+				if (inRest) {
+					// Rest continues
+					currentLength++;
+					
+				} else {
+					// New rest
+					int reducedLength = currentLength / gcf;
+					
+					// Add initial symbol (complex in case pattern begins with a TIE)
+					reducedPattern.add(reducedPattern.isEmpty() ? beatQuantum.get(0) : MetricalLpcfgQuantum.ONSET);
+					
+					// Add all ties
+					for (int j = 1; j < reducedLength; j++) {
+						reducedPattern.add(MetricalLpcfgQuantum.TIE);
+					}
+					
+					inRest = true;
+					currentLength = 1;
+				}
+				break;
+				
+			case ONSET:
+				// New note
+				int reducedLength = currentLength / gcf;
+
+				if (inRest) {
+					// Add all RESTs
+					for (int j = 0; j < reducedLength; j++) {
+						reducedPattern.add(MetricalLpcfgQuantum.REST);
+					}
+					
+				} else {
+					// Add initial symbol (complex in case pattern begins with a TIE)
+					reducedPattern.add(reducedPattern.isEmpty() ? beatQuantum.get(0) : MetricalLpcfgQuantum.ONSET);
+				
+					// Add all TIEs
+					for (int j = 1; j < reducedLength; j++) {
+						reducedPattern.add(MetricalLpcfgQuantum.TIE);
+					}
+				}
+				
+				currentLength = 1;
+				inRest = false;
+				break;
+				
+			case TIE:
+				if (inRest) {
+					System.err.println("ERROR: TIE after REST - Treating as ONSET");
+					
+					reducedLength = currentLength / gcf;
+					
+					for (int j = 0; j < reducedLength; j++) {
+						reducedPattern.add(MetricalLpcfgQuantum.REST);
+					}
+					
+					currentLength = 1;
+					inRest = false;
+					
+				} else {
+					// Note continues
+					currentLength++;
+				}
+				break;
+			}
+		}
+		
+		// Handle final constituent
+		int reducedLength = currentLength / gcf;
+
+		if (inRest) {
+			// Add all RESTs
+			for (int j = 0; j < reducedLength; j++) {
+				reducedPattern.add(MetricalLpcfgQuantum.REST);
+			}
+			
+		} else {
+			// Add initial symbol (complex in case pattern begins with a TIE)
+			reducedPattern.add(reducedPattern.isEmpty() ? beatQuantum.get(0) : MetricalLpcfgQuantum.ONSET);
+		
+			// Add all TIEs
+			for (int j = 1; j < reducedLength; j++) {
+				reducedPattern.add(MetricalLpcfgQuantum.TIE);
+			}
+		}
+		
+		return reducedPattern;
+	}
+	
+	/**
+	 * Get the greatest common factor of the lengths of all of the constituents in the given
+	 * pattern.
+	 * 
+	 * @param beatQuantum The pattern we want to reduce.
+	 * @return The greatest common factor of the constituents of the given pattern.
+	 */
+	private static int getGCF(List<MetricalLpcfgQuantum> beatQuantum) {
+		// Find constituent lengths
+		List<Integer> lengths = new ArrayList<Integer>();
+		lengths.add(1);
+		
+		int gcf = 0;
+		
+		// Are we initially in a rest?
+		boolean inRest = beatQuantum.get(0) == MetricalLpcfgQuantum.REST;
+		
+		for (int i = 1; i < beatQuantum.size(); i++) {
+			if (gcf == 1) {
+				return 1;
+			}
+			
+			switch (beatQuantum.get(i)) {
+			case REST:
+				if (inRest) {
+					// Rest continues
+					incrementLast(lengths);
+					
+				} else {
+					// New rest
+					inRest = true;
+					int lastLength = lengths.get(lengths.size() - 1);
+					gcf = gcf == 0 ? lastLength : MathUtils.getGCF(gcf, lastLength);
+					lengths.add(1);
+				}
+				break;
+				
+			case ONSET:
+				// New note
+				int lastLength = lengths.get(lengths.size() - 1);
+				gcf = gcf == 0 ? lastLength : MathUtils.getGCF(gcf, lastLength);
+				lengths.add(1);
+				inRest = false;
+				break;
+				
+			case TIE:
+				if (inRest) {
+					System.err.println("ERROR: TIE after REST - Treating as ONSET");
+					lastLength = lengths.get(lengths.size() - 1);
+					gcf = gcf == 0 ? lastLength : MathUtils.getGCF(gcf, lastLength);
+					lengths.add(1);
+					inRest = false;
+					
+				} else {
+					// Note continues
+					incrementLast(lengths);
+				}
+				break;
+			}
+		}
+		
+		// Add last constituent (if we already did, it won't affect the gcf anyways)
+		int lastLength = lengths.get(lengths.size() - 1);
+		gcf = gcf == 0 ? lastLength : MathUtils.getGCF(gcf, lastLength);
+		
+		return gcf;
+	}
+	
+	/**
+	 * Utility method to increment the last value in an Integer List.
+	 * 
+	 * @param list The Integer List whose last value we want to increment.
+	 */
+	private static void incrementLast(List<Integer> list) {
+		if (list.isEmpty()) {
+			return;
+		}
+		
+		list.set(list.size() - 1, list.get(list.size() - 1) + 1);
+	}
+
+	/**
+	 * 
+	 * 
+	 * @param anchorBeats
+	 * @param numTimes
+	 * @param nextTime
+	 * @param defaultLength
+	 * @return
+	 */
+	private static List<Long> getAnchoredTimes(List<Beat> anchorBeats, int numTimes, long nextTime, long defaultLength) {
+		List<Long> times = new ArrayList<Long>(numTimes);
+		
+		int numAnchors = anchorBeats.size();
+		
+		if (numTimes % numAnchors == 0) {
+			// Anchors are valid here. Make sure we align with them.
+			
+			int timesPerAnchor = numTimes / numAnchors;
+			
+			for (int anchorNum = 0; anchorNum < numAnchors; anchorNum++) {
+				long anchorTime = anchorBeats.get(anchorNum).getTime();
+				long nextAnchorTime = anchorNum == numAnchors - 1 ? nextTime : anchorBeats.get(anchorNum + 1).getTime();
+				
+				double diff = nextAnchorTime == Long.MAX_VALUE ? defaultLength : ((double) nextAnchorTime - anchorTime) / timesPerAnchor;
+				defaultLength = (long) diff;
+				
+				for (int i = 0; i < timesPerAnchor; i++) {
+					times.add(Math.round(anchorTime + i * diff));
+				}
+			}
+			
+		} else {
+			// The anchors weren't valid. Just split evenly.
+			double diff = nextTime == Long.MAX_VALUE ? defaultLength : ((double) nextTime - anchorBeats.get(0).getTime()) / numTimes;
+			
+			for (int i = 0; i < numTimes; i++) {
+				times.add(Math.round(anchorBeats.get(0).getTime() + i * diff));
+			}
+		}
+		
+		return times;
 	}
 
 	/**
@@ -247,14 +489,12 @@ public class MetricalLpcfgTreeFactory {
 	 * @param notes The notes that might lie within this beat.
 	 * @param hasStarted True if some note has already occurred in this bar. False otherwise. This is used
 	 * to handle extending notes.
+	 * @param subBeatLength
 	 * 
 	 * @return The {@link MetricalLpcfgNonterminal} representing a Beat with the given properties.
 	 */
 	private static MetricalLpcfgNode makeBeat(long prevTime, List<Long> times, long nextTime,
-			List<MidiNote> notes, boolean hasStarted) {
-		
-		// TODO: Try with 3 or 4 tatums per bar
-		
+			List<MidiNote> notes, boolean hasStarted, int subBeatLength) {
 		MetricalLpcfgNonterminal beat = new MetricalLpcfgNonterminal(MetricalLpcfgLevel.BEAT);
 		
 		List<MetricalLpcfgQuantum> quantums = new ArrayList<MetricalLpcfgQuantum>(Collections.nCopies(times.size(),
@@ -299,10 +539,10 @@ public class MetricalLpcfgTreeFactory {
 		}
 		
 		// Can't reduce to 1. Make each sub-beat individually.
-		int numSubBeats = quantums.size() / SUB_BEAT_LENGTH;
-		for (int subBeatIndex = 0; subBeatIndex < quantums.size(); subBeatIndex += SUB_BEAT_LENGTH) {
+		int numSubBeats = quantums.size() / subBeatLength;
+		for (int subBeatIndex = 0; subBeatIndex < quantums.size(); subBeatIndex += subBeatLength) {
 			MetricalLpcfgNonterminal subBeat = new MetricalLpcfgNonterminal(MetricalLpcfgLevel.SUB_BEAT);
-			subBeat.addChild(new MetricalLpcfgTerminal(quantums.subList(subBeatIndex, subBeatIndex + SUB_BEAT_LENGTH), 1, numSubBeats));
+			subBeat.addChild(new MetricalLpcfgTerminal(quantums.subList(subBeatIndex, subBeatIndex + subBeatLength), 1, numSubBeats));
 			
 			beat.addChild(subBeat);
 		}
